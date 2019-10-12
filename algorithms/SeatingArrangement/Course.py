40403f69f5414062b58f11f089206b179e27a567
Sohail Rajdev <sohailrajdev97@gmail.com>
2019-10-12 15:44:38 +0530

    algorithm: add SeatingArrangement algorithm

---
+++ algorithms/SeatingArrangement/Course.py
@@ -0,0 +1,74 @@
+import os
+
+
+class Course:
+    def __init__(self, code, name, exam_start, exam_end):
+        self.code = code
+        self.name = name
+        self.students = []
+        # Array of tuples of format (Room Number, Student Count)
+        self.rooms = []
+        self.exam_start = exam_start
+        self.exam_end = exam_end
+
+    def get_exam_slices(self):
+
+        slices = []
+        start_index = 0
+
+        for room in self.rooms:
+
+            if start_index >= len(self.students):
+                break
+
+            end_index = start_index + \
+                room[1] - 1 if start_index + room[1] - \
+                1 < len(self.students) else -1
+            slices.append(
+                (room[0], room[1], self.students[start_index], self.students[end_index]))
+            start_index += room[1]
+
+        if start_index != len(self.students):
+            print(
+                f"Total seated students different from students enrolled for {self.code}{os.linesep}")
+            print(
+                f"\tSeated: {start_index}{os.linesep}\tEnrolled: {len(self.students)}\n")
+
+        return slices
+
+    def __repr__(self):
+        return f"{self.code} | {self.name}"
+
+
+class CourseList:
+    def __init__(self):
+        self.courses = []
+
+    def add_course(self, code, name, exam_start, exam_end):
+        self.courses.append(Course(code, name, exam_start, exam_end))
+
+    def add_if_not_exists(self, code, name, exam_start, exam_end):
+        if self.find_by_code(code) is None:
+            self.add_course(code, name, exam_start, exam_end)
+
+    def find_by_code(self, code):
+        for course in self.courses:
+
+            splitted = course.code.split("/")
+
+            for splitted_course in splitted:
+                if code.strip() == splitted_course.strip():
+                    return course
+
+        return None
+
+    def sort_entries(self):
+
+        self.courses.sort(key=lambda x: x.code)
+
+        for course in self.courses:
+            course.rooms.sort()
+            course.students.sort()
+
+    def __repr__(self):
+        return f"{len(self.courses)} courses"
---
+++ algorithms/SeatingArrangement/main.py
@@ -0,0 +1,133 @@
+import datetime
+from Course import CourseList
+from openpyxl import Workbook
+from openpyxl.styles import Alignment, Font
+
+
+def get_dates_from_key(key):
+
+    splitted = key.split("|")
+
+    start = datetime.datetime.strptime(
+        splitted[0].strip() + " " + splitted[1].strip(), "%d/%m/%Y %H:%M")
+    end = datetime.datetime.strptime(
+        splitted[0].strip() + " " + splitted[2].strip(), "%d/%m/%Y %H:%M")
+
+    return (start, end)
+
+
+def is_no_exam_code(code):
+
+    no_exam_codes = ["F266", "F366", "F367", "F376", "F377"]
+
+    for ne_code in no_exam_codes:
+        if ne_code in code:
+            return True
+
+    if code[-1] == "T":
+        return True
+
+    return False
+
+
+def get_course_list(file_name):
+
+    f = open(file_name)
+
+    course_list = CourseList()
+
+    for line in f.readlines():
+
+        splitted = line.split(",")
+        room = splitted[0]
+        code = splitted[1]
+        name = splitted[2]
+        student_count = int(splitted[4])
+        start, end = get_dates_from_key(splitted[6])
+
+        course_list.add_if_not_exists(code, name, start, end)
+        course = course_list.find_by_code(code)
+        course.rooms.append((room, student_count))
+
+    f.close()
+
+    return course_list
+
+
+def add_students(course_list, file_name):
+
+    f = open(file_name)
+
+    invalid_courses = set()
+
+    for line in f.readlines():
+
+        splitted = line.strip().split(",")
+        student_id = splitted[0]
+        course_code = splitted[1]
+        course = course_list.find_by_code(course_code)
+
+        if course is not None:
+            if student_id not in course.students:
+                course.students.append(student_id)
+
+        else:
+            if not is_no_exam_code(course_code):
+                invalid_courses.add(course_code)
+
+    f.close()
+
+    return invalid_courses
+
+
+def export_xlsx(course_list, file_name):
+
+    wb = Workbook()
+    ws = wb.active
+    current_row = 1
+
+    for course in course_list.courses:
+
+        if len(course.students) == 0:
+            print(
+                f"No students enroled in {course.code}. Please re-check Registered Students list.")
+            continue
+
+        ws[f"A{current_row}"] = f"{course.code}"
+        ws[f"B{current_row}"] = f"{course.name}"
+
+        date_string = f"{course.exam_start.strftime('%d %B %Y - %I:%M %p')} to {course.exam_end.strftime('%I:%M %p')}"
+        ws[f"C{current_row}"] = date_string
+
+        exam_slices = course.get_exam_slices()
+        ws[f"D{current_row}"] = exam_slices[0][0]
+        ws[f"E{current_row}"] = exam_slices[0][2] + " to " + exam_slices[0][3]
+        ws[f"F{current_row}"] = exam_slices[0][1]
+
+        current_row += 1
+
+        for exam_slice in exam_slices[1:]:
+            ws[f"D{current_row}"] = exam_slice[0]
+            ws[f"E{current_row}"] = exam_slice[2] + " to " + exam_slice[3]
+            ws[f"F{current_row}"] = exam_slice[1]
+            current_row += 1
+
+    wb.save(file_name)
+
+
+def start_process(room_allotment_csv, students_csv):
+    course_list = get_course_list(room_allotment_csv)
+
+    invalid_courses = add_students(course_list, students_csv)
+
+    print("The following courses do not have a room allotment (please re-confirm):\n")
+
+    for course in invalid_courses:
+        print(f"\t=> {course}")
+
+    course_list.sort_entries()
+
+    export_xlsx(
+        course_list, "./SeatingArrangement.xlsx")
+    
+    print("Seating Arrangement exported to SeatingArrangement.xlsx")
