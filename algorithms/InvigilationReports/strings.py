f0a849afad63680eb70baf2d93fe6f79ee0770d4
Anirudh-idk <aniragg30@gmail.com>
2024-01-15 14:55:34 +0530

    fix: change path definiton to work with exe file

--- algorithms/InvigilationReports/strings.py
+++ algorithms/InvigilationReports/strings.py
@@ -1,7 +1,20 @@
 from bs4 import BeautifulSoup
+import os, sys
 
-f = open("./invigilation_reports_config.xml")
-parsed = BeautifulSoup(f, 'xml')
+
+def resource_path(relative_path):
+    """Get absolute path to resource, works for dev and for PyInstaller"""
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
+f = open(resource_path("invigilation_reports_config.xml"))
+parsed = BeautifulSoup(f, "xml")
 
 college_name = parsed.college.text
 office_name = parsed.office.text
