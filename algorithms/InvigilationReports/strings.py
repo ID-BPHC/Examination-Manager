a87f2629d4e1d13ccbf98d513165cc0e0b64ccf8
Anirudh-idk <aniragg30@gmail.com>
2025-01-29 11:14:11 +0530

    chore: add strings for room captains and group captains

--- algorithms/InvigilationReports/strings.py
+++ algorithms/InvigilationReports/strings.py
@@ -16,3 +16,11 @@ invigilator_notes = [x.text for x in parsed.invigilator.notes.findAll("note")]
 ic_greeting = parsed.ic.greeting.text
 ic_intro = parsed.ic.intro.text
 ic_notes = [x.text for x in parsed.ic.notes.findAll("note")]
+
+room_captain_greeting = parsed.room_captain.greeting.text
+room_captain_intro = parsed.room_captain.intro.text
+room_captain_notes = [x.text for x in parsed.room_captain.notes.findAll("note")]
+
+group_captain_greeting = parsed.group_captain.greeting.text
+group_captain_intro = parsed.group_captain.intro.text
+group_captain_notes = [x.text for x in parsed.group_captain.notes.findAll("note")]
